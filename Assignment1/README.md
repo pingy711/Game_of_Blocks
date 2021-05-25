555d8c54a5c8ca0eb7ae66db40f47385fd05e898
pingy711 <aditya0subramanian@gmail.com>
2021-05-25 19:44:36 +0530

    Add final submission

--- Assignment1/README.md
+++ Assignment1/README.md
@@ -1,1 +1,11 @@
-I'm new to Javascript and I dint understand how to use functions from libraries. I'm submitting what I have compiled from various online sources and what I have written as of now, it is extremely rough and i still need to work on it after learning more of Javascript. (You can ignore the .cpp file). I'm submitting this becuase the deadline is near.
+I have built and debugged this code on node-js.
+
+How to call the program? If you want to run the program with some input string (let's denote it by {input}) then we need to write the following command on your terminal:
+	node assignment1.js {input}
+
+This code works for all kind of input strings, with or without spaces in between.
+
+The output is in 3 lines:
+The first line will show your hexadecimal value that starts with 5 zeros. The second line shows the value that has to be appended after the input string in order to get the desired output. The third line shows the program execuion time.
+
+P.S the html file has no real purpose in the demonstration of this code and can be ignored without problem.
--- Assignment1/assignment_1.js
+++ Assignment1/assignment_1.js
@@ -1,9 +1,47 @@
-const crypto = require('crypto-js');
-var hash = crypto.createHash('sha256');
-var code = 'bacon';
+console.time("executionTime");
+var crypto = require('crypto');
+const { EOF } = require('dns');
+const { exit } = require('process');
+var code = process.argv[2];
+var i = 3;
+while(process.argv[i]!=null){
+    code = code + ' ';
+    code = code.concat(process.argv[i]);++i;}
 console.log(code);
-code = hash.update(code);
-console.log(code);
-code = hash.digest(code);
-console.log(code);
-//import sha256 from 'crypto-js/sha256';
+var x = 2;
+{
+    const hash_0 = crypto.createHash('sha256').update(code).digest('hex');
+    if(hash_0 <='0x0000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF')
+        {
+            console.log(code);
+            console.log('Nothing to be appended');
+            return ;
+        }
+}
+code = code.concat(String(1));
+while(true)
+{
+    //const hash = crypto.createHash('sha256');                         //initially written,does not give 'hash' as hexadecimal but returns as an object
+    //var code_hashed = hash.update(code);                              //initially written,does not give 'hash' as hexadecimal but returns as an object
+    //code_hashed = hash.digest(code_hashed);                           //initially written,does not give 'hash' as hexadecimal but returns as an object
+    const hash = crypto.createHash('sha256').update(code).digest('hex');
+    var temp = String(hash).slice(0,-59);
+    if(temp=='00000')
+        {                                                                       
+            console.log(hash);
+            console.log(x-1);
+            console.timeEnd("executionTime");
+            return ;
+        }
+   // console.log(Math.floor(Math.log10(x-1))+1);               //used while debugging
+    code = code.slice(0,-(Math.floor(Math.log10(x-1))+1));                //removes the concatenated x-1 from 'code'
+    //console.log(code);                                    //used while debugging
+    code = code.concat(String(x));                                        //appends x to 'code'
+    //console.log(code);                        //used while debugging
+    //console.log(x);                          //used while debugging
+    //console.log(hash);                       //used while debugging
+    //console.log('\n');                       //used while debugging
+    x++;                   
+}
+//const end_time = window.performance.now();
+//console.log(end_time-start_time);
