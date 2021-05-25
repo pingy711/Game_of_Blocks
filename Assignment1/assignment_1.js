e03a74f9399d52136a1372ed024c4e1d9f2c6a39
pingy711 <aditya0subramanian@gmail.com>
2021-05-25 14:57:52 +0530

    Add

--- Assignment1/assignment_1.js
+++ Assignment1/assignment_1.js
@@ -1,33 +1,9 @@
-var CryptoJS = require("crypto-js");
+const crypto = require('crypto-js');
+var hash = crypto.createHash('sha256');
+var code = 'bacon';
+console.log(code);
+code = hash.update(code);
+console.log(code);
+code = hash.digest(code);
+console.log(code);
 //import sha256 from 'crypto-js/sha256';
-var SHA256 = require("crypto-js/sha256");
-var name = 'Aditya';
-console.log(SHA256(name));
-//console.log(sha256(name));
-
-public static String calculateHMAC(String data, String key) throws Exception {
-    String result;
-    try {
-        // get an hmac_sha2 key from the raw key bytes
-        SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(), HMAC_SHA2_ALGORITHM);
-    
-        // get an hmac_sha1 Mac instance and initialize with the signing key
-        Mac sha256_HMAC = Mac.getInstance(HMAC_SHA2_ALGORITHM);
-        sha256_HMAC.init(signingKey);
-    
-        // compute the hmac on input data bytes
-        byte[] rawHmac = sha256_HMAC.doFinal(data.getBytes());
-    
-        // base64-encode the hmac 
-        StringBuilder sb = new StringBuilder();
-        char[] charArray = Base64.encode(rawHmac);
-            for ( char a : charArray){
-                sb.append(a);
-                }
-            result = sb.toString();
-        }
-        catch (Exception e) {
-            throw new SignatureException("Failed to generate HMAC : " + e.getMessage());
-        }
-        return result;
-    }
